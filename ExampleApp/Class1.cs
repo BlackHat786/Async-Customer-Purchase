de64965f52d5e3ace4f2566f7d5af04161c048e7
Mohammed Ameen Abdool <21612147@dut4life.ac.za>
2019-07-24 23:39:42 +0200

    Add project files.

---
+++ ExampleApp/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp
+{
+    public class Class1
+    {
+
+        async Task<double> NumItems()
+        {
+            int numItems = 0;
+            string NameItems = "";
+            double price = 0.0, discount = 0.05, total = 0.0;
+            Console.Write("Enter Number of items purchased: ");
+            numItems = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numItems; i++)
+            {
+                Console.Write("Enter the name of Item purchased: ");
+                NameItems = Console.ReadLine();
+
+                Console.Write("Enter The Price of Item: ");
+                price = Convert.ToDouble(Console.ReadLine());
+
+                total += price;
+
+            }
+
+            if (total > 20)
+            {
+                total = total - (total * discount);
+            }
+
+            await Task.Delay(1000);
+            Console.Write("Total Price: R" + total);
+            return total;
+
+        }
+
+       public void display()
+        {
+            string name = "", sname = "";
+
+            Console.Write("Enter customer's name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter the customer's Surname: ");
+            sname = Console.ReadLine();
+            NumItems();
+
+        }
+
+    }
+}
